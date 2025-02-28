49087cafe8e25f5c4cd4d192effb9a2296ca1114
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2025-02-28 03:48:52 -0500

    Added Debug Get Log

--- src/functions/cpcstorageeventtrigger.ts
+++ src/functions/cpcstorageeventtrigger.ts
@@ -1,7 +1,21 @@
 import { app, EventGridEvent, InvocationContext } from "@azure/functions";
 
+async function fetchData() {
+    try {
+        const response = await fetch("https://api.tent.brandontinder.info/");
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log("Response Data:", data);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
 export async function cpcstorageeventtrigger(event: EventGridEvent, context: InvocationContext): Promise<void> {
     context.log('Event grid function processed event:', event);
+    fetchData();
 }
 
 app.eventGrid('cpcstorageeventtrigger', {
